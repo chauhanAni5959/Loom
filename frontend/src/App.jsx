e9f4a5a1e1c69210a3188e5ea4810baadde63861
anisingh <anisingh565072@gmail.com>
2026-05-15 17:44:15 +0530

    created pages for routing

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,24 @@
-import React from "react";
+import Routes, { Route } from "react-router";
+import HomePage from "./pages/HomePage";
+import SignUpPage from "./pages/SignUpPage";
+import LoginPage from "./pages/LoginPage";
+import Notifications from "./pages/Notifications";
+import CallPage from "./pages/CallPage";
+import ChatPage from "./pages/ChatPage";
+import OnBoardingPage from "./pages/OnBoardingPage";
 
 const App = () => {
   return (
-    <div className=" h-screen text-3xl" data-theme="light">
-      
+    <div className="h-screen" data-theme="night">
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/signup" element={<SignUpPage />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/notifications" element={<Notifications />} />
+        <Route path="/call" element={<CallPage />} />
+        <Route path="/chat" element={<ChatPage />} />
+        <Route path="/onboarding" element={<OnBoardingPage />} />
+      </Routes>
     </div>
   );
 };
---
+++ frontend/src/pages/Notifications.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Notifications = () => {
+  return (
+    <div>
+      Notification
+      
+    </div>
+  )
+}
+
+export default Notifications
