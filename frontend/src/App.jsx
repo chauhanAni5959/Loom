fb1abd56f220f458135028fd65f68a22d188f7eb
anisingh <anisingh565072@gmail.com>
2026-05-15 07:42:15 +0530

    created themes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,14 +1,9 @@
+import React from "react";
+
 const App = () => {
   return (
-    <div className="bg-red-500">
-      <button className="btn btn-neutral">Neutral</button>
-      <button className="btn btn-primary">Primary</button>
-      <button className="btn btn-secondary">Secondary</button>
-      <button className="btn btn-accent">Accent</button>
-      <button className="btn btn-info">Info</button>
-      <button className="btn btn-success">Success</button>
-      <button className="btn btn-warning">Warning</button>
-      <button className="btn btn-error">Error</button>
+    <div className=" h-screen text-3xl" data-theme="light">
+      
     </div>
   );
 };
